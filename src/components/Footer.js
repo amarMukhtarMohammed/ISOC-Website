c325d72a6b7ae8da95731349c885cc984e7cfbb3
Amar <amarmukhtar398@gmail.com>
2022-10-13 12:54:08 +0100

    Modified Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -35,16 +35,6 @@ const Footer = () => {
                       </Link>
                     </li>
                     <li>
-                      <Link className="navbar-item" to="/products">
-                        Products
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact/examples">
-                        Form Examples
-                      </Link>
-                    </li>
-                    <li>
                       <a
                         className="navbar-item"
                         href="/admin/"
@@ -61,11 +51,6 @@ const Footer = () => {
                 <section>
                   <ul className="menu-list">
                     <li>
-                      <Link className="navbar-item" to="/blog">
-                        Latest Stories
-                      </Link>
-                    </li>
-                    <li>
                       <Link className="navbar-item" to="/contact">
                         Contact
                       </Link>
@@ -81,25 +66,10 @@ const Footer = () => {
                     style={{ width: "1em", height: "1em" }}
                   />
                 </a>
-                <a title="twitter" href="https://twitter.com">
-                  <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: "1em", height: "1em" }}
-                  />
-                </a>
                 <a title="instagram" href="https://instagram.com">
                   <img
                     src={instagram}
                     alt="Instagram"
-                    style={{ width: "1em", height: "1em" }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
                     style={{ width: "1em", height: "1em" }}
                   />
                 </a>
